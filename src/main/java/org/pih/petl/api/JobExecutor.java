38e524d9cb0e0ff8149e7291b5a7ade43f8398d9
Michael Seaton <mseaton@pih.org>
2021-10-22 14:14:18 -0400

    PETL-106: Improved error handling when using job executor

--- src/main/java/org/pih/petl/api/JobExecutor.java
+++ src/main/java/org/pih/petl/api/JobExecutor.java
@@ -1,5 +1,6 @@
 package org.pih.petl.api;
 
+import org.hibernate.query.criteria.internal.expression.function.AggregationFunction;
 import org.pih.petl.job.config.ExecutionConfig;
 
 import java.util.List;
@@ -13,19 +14,43 @@ import java.util.concurrent.Future;
  */
 public class JobExecutor {
 
+    public static final Integer MAX_ALLOWED_THREADS = 10;
+
     public static void execute(List<JobExecutionTask> tasks, ExecutionContext context, ExecutionConfig config) throws InterruptedException, ExecutionException {
         context.setStatus("Executing tasks");
-        ExecutorService executorService = Executors.newFixedThreadPool(config.getMaxConcurrentJobs());
-        List<Future<JobExecutionResult>> results = executorService.invokeAll(tasks);
-        executorService.shutdown();
 
-        // We only arrive here once all Tasks have been completed
+        int maxJobs = config.getMaxConcurrentJobs() == null ? 1 : (config.getMaxConcurrentJobs() < 0 ? MAX_ALLOWED_THREADS : config.getMaxConcurrentJobs());
 
-        for (Future<JobExecutionResult> resultFutures : results) {
-            JobExecutionResult result = resultFutures.get();
-            if (!result.isSuccessful()) {
-                throw new RuntimeException("Error in one of the jobs", result.getException());
+        if (maxJobs != 0) {
+            if (maxJobs == 1) {
+                // If only one job should be executed at a time, then this means we want to process in serial, not parallel,
+                // and we want to process each task and block on executing others unless tasks before execute successfully without exception
+                for (JobExecutionTask task : tasks) {
+                    Future<JobExecutionResult> resultFuture = Executors.newSingleThreadExecutor().submit(task);
+                    JobExecutionResult result = resultFuture.get(); // This will throw an exception if one thrown in the executed task
+                    if (!result.isSuccessful()) {
+                        throw new RuntimeException("An error occurred executing a job", result.getException());
+                    }
+                }
+            } else {
+                // Otherwise, if more than one job is configured to execute at a time, assume this means we want to execute in parallel
+                // And continue processing other jobs even if one or more of the jobs fail
+                ExecutorService executorService = Executors.newFixedThreadPool(config.getMaxConcurrentJobs());
+                List<Future<JobExecutionResult>> results = executorService.invokeAll(tasks);
+                executorService.shutdown();
+
+                // We only arrive here once all Tasks have been completed
+
+                for (Future<JobExecutionResult> resultFutures : results) {
+                    JobExecutionResult result = resultFutures.get();
+                    if (!result.isSuccessful()) {
+                        throw new RuntimeException("Error in one of the jobs", result.getException());
+                    }
+                }
             }
+        }
+        else {
+            context.setStatus("Not executing job since it is configured with maxConcurrentJobs=0");
         }
     }
 
