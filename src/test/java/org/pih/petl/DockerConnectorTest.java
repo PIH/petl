5abceb63d8130a9f55c7b7a58a751acbc767cfe8
Michael Seaton <mseaton@pih.org>
2023-11-07 20:27:30 -0500

    Add ability to start and stop a database docker container for use while job is executing.

--- src/test/java/org/pih/petl/DockerConnectorTest.java
+++ src/test/java/org/pih/petl/DockerConnectorTest.java
@@ -1,15 +1,11 @@
 package org.pih.petl;
 
 import com.github.dockerjava.api.model.Container;
-import org.junit.Test;
-import org.junit.jupiter.api.Disabled;
 
 import java.util.Arrays;
 
-@Disabled
 public class DockerConnectorTest {
 
-    @Test
     public void testIsRunning() {
         try (DockerConnector docker = DockerConnector.open()) {
             for (Container container : docker.getContainers()) {
