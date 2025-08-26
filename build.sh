5bd81b7174f80e6ef6d794c366d83d08e97dcb74
Michael Seaton <mseaton@pih.org>
2025-08-26 09:03:27 -0400

    PETL-134 - Add ability to build PETL using Docker

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -eux
 
-MAVEN_ARGS="${1:-clean verify}"
-
-docker run --rm -v .:/data -v /var/run/docker.sock:/var/run/docker.sock partnersinhealth/petl-maven-build ${MAVEN_ARGS}
+docker run --rm -v .:/data -v /var/run/docker.sock:/var/run/docker.sock partnersinhealth/petl-maven-build $@
